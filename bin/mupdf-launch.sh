f52c08a0391d8ba4575d0eabb2e2a0054ee68e5a
NFVblog <66777517+NF02@users.noreply.github.com>
2022-03-24 12:02:11 +0100

    zathura fix

--- bin/mupdf-launch.sh
+++ bin/mupdf-launch.sh
@@ -5,12 +5,12 @@ pdffile="$1"
 zathura "$pdffile" &>/dev/null &
 mupid="$!"
 muwinid="0"
-nummps=$(xdotool search --class "MuPDF" getwindowname %@ | grep -n "$pdffile" | cut -d":" -f1)
+nummps=$(xdotool search --class "Zathura" getwindowname %@ | grep -n "$pdffile" | cut -d":" -f1)
 while [ -z $nummps ] ; do
     sleep 0.1
-    nummps=$(xdotool search --class "MuPDF" getwindowname %@ | grep -n "$pdffile" | cut -d":" -f1)
+    nummps=$(xdotool search --class "Zathura" getwindowname %@ | grep -n "$pdffile" | cut -d":" -f1)
 done
-echo -n "$(xdotool search --class "MuPDF" | sed -n "$nummps p")"
+echo -n "$(xdotool search --class "Zathura" | sed -n "$nummps p")"
 # try to return focus to GVIM
 xdotool search --class --name "${texfile}.*GVIM" windowactivate &>/dev/null
 exit 0
